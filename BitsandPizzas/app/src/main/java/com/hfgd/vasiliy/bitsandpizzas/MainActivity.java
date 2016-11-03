aa57e7534111332f480d0127ab009a6aa4e01c35
Vasiliy Kirstia <vailiykirstia@yandex.ru>
2016-11-03 11:53:45 +0200

    Страница 448.

--- BitsandPizzas/app/src/main/java/com/hfgd/vasiliy/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/com/hfgd/vasiliy/bitsandpizzas/MainActivity.java
@@ -1,20 +1,46 @@
 package com.hfgd.vasiliy.bitsandpizzas;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ShareActionProvider;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
     private ShareActionProvider shareActionProvider;
+    private String[] titles;
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            selectItem(i);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titles = this.getResources().getStringArray(R.array.titles);
+        drawerList = (ListView)findViewById(R.id.drawer);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, titles));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        if(savedInstanceState == null)
+            selectItem(0);
     }
 
     @Override
@@ -47,5 +73,41 @@ public class MainActivity extends Activity {
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, text);
         this.shareActionProvider.setShareIntent(intent);
+    }
+
+    private void selectItem(int position){
+        Fragment fragment;
+        switch(position){
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoresFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+                break;
+        }
+
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
+
+        setActionBarTitle(position);
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    private void setActionBarTitle(int position){
+        String title;
+        if(position == 0)
+            title = getResources().getString(R.string.app_name);
+        else
+            title = titles[position];
+        getActionBar().setTitle(title);
     }
 }
