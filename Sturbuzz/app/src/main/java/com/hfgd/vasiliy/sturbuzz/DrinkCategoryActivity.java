d3dd3a574611e595dafb05cf40902f2e034164bd
VasiliyKirstia <vasiliykirstia@yandex.ru>
2016-11-09 09:39:00 +0200

    Page 539

--- Sturbuzz/app/src/main/java/com/hfgd/vasiliy/sturbuzz/DrinkCategoryActivity.java
+++ Sturbuzz/app/src/main/java/com/hfgd/vasiliy/sturbuzz/DrinkCategoryActivity.java
@@ -2,19 +2,41 @@ package com.hfgd.vasiliy.sturbuzz;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends ListActivity {
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(this, android.R.layout.simple_list_item_1, Drink.drinks);
-        this.setListAdapter(listAdapter);
+        try{
+            SQLiteOpenHelper starbuzzDatabaseHelper = new SturbuzzDatabaseHelper(this);
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+
+            cursor = db.query("drink", new String[]{"_id", "name"}, null, null, null, null, null);
+            CursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"name"},
+                    new int[]{android.R.id.text1},
+                    0);
+            setListAdapter(listAdapter);
+        }catch(SQLiteException e){
+            Toast toast = Toast.makeText(this, "Database unavalible", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
     @Override
@@ -25,5 +47,12 @@ public class DrinkCategoryActivity extends ListActivity {
         intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int)id);
 
         this.startActivity(intent);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
